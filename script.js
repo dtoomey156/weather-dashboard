88457caf4376a1d438ec0c189db4c904d12442bc
Daniel Toomey <danieltoomey@Daniels-iMac.lan>
2021-03-30 22:11:20 -0700

    display city and temp in current weather card on input and button click

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// sets up function for the document to listen to events 
+
+$(document).ready(function() {
+
+// jquery to store the city-input field to a variable on a button click
+
+    $("#citySearchBtn").on("click", function() {
+        var weatherSearch = $("#city-input").val()
+
+        // logs city to console on button click
+        console.log(weatherSearch)
+
+        // TODO - make a button and append to the history section to search city history
+        getWeather(weatherSearch)
+    })
+
+    var getWeather = function(city) {
+        // format the github api url
+        var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5a03c4b84b5e9bfac4ce287b162bcdec";
+    
+        //make a request to the url
+        fetch(apiUrl).then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+                $("#weatherContainer").empty()
+                var currentWeather = $("<div>").addClass("card-body")
+                var cityName = $("<h3>").addClass("card-body").text(data.name)
+                currentWeather.append(cityName)
+                var temp = $("<p>").addClass("card-text").text(data.main.temp)
+                currentWeather.append(temp)
+
+
+
+                
+                // TODO add additional items in the weather container before appending the weatherContainer    
+
+
+                $("#weatherContainer").append(currentWeather)
+            });
+        });
+    };
+
+});
+
+
+
+
+//fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5a03c4b84b5e9bfac4ce287b162bcdec").then(function(response) {
+  //  response.json().then(function(data) {
+    //    console.log(data);
+    //});
+//});
